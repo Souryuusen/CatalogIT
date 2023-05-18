9c72375fa17d2aaac9b305db878c38e94c9b5c67
GL <g.l@gmail.com>
2023-05-18 20:51:06 +0200

    - Code Clean-ups - removed 75 warnings

---
+++ src/main/java/com/souryuu/catalogit/exception/NoElementSelectedFromPageException.java
@@ -0,0 +1,8 @@
+package com.souryuu.catalogit.exception;
+
+public class NoElementSelectedFromPageException extends RuntimeException{
+
+    public NoElementSelectedFromPageException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/souryuu/catalogit/exception/ViewLoadException.java
@@ -0,0 +1,8 @@
+package com.souryuu.catalogit.exception;
+
+public class ViewLoadException extends RuntimeException{
+
+    public ViewLoadException(String message) {
+        super(message);
+    }
+}
